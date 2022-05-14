9c3236563f550e176c14c5af7ed63aa7fbecadcc
anilkumar9622 <anilchauhan.src@gmail.com>
2022-05-14 18:59:17 +0530

    ggg

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
-
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(express.static(path.join(__dirname)));
-app.use("/styles", express.static(__dirname));
-app.use("/images", express.static(__dirname + '/images'));
-app.use("/scripts", express.static(__dirname + '/scripts'));
-
-// viewed at based directory http://localhost:8080/
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'views/index.html'));
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname,'build');
+const port = process.env.PORT || 5000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-// add other routes below
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname + 'views/about.html'));
+app.listen(port, () => {
+   console.log('Server is up!');
 });
-
-app.listen(process.env.PORT || 8080);
